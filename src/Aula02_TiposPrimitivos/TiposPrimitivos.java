148b18a20cfbde3e1cb7a26d268df8e5b260e566
Edgard <edgardmoraes@icloud.com>
2025-06-24 16:47:44 -0300

    feat:Aula02_Tipos_Primitivos

---
+++ src/Aula02_TiposPrimitivos/TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package Aula02_TiposPrimitivos;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        //  int,double, float, chat, byte, short, long, boolean ( todos os tipos primitivos são escritas com a primeira
+        //  letra minuscula)
+        int idade = 10;
+        long salarioGrande = 10000;
+        double salarioDouble = 2000;
+        float salarioFloat = 3000;
+        byte idadeByte = 10;
+        short idadeShort = 20;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+        String nome = "goku";
+
+
+
+
+    }
+}
---
+++ src/Aula02_TiposPrimitivos/TiposPrimitivosExercicio.java
@@ -0,0 +1,20 @@
+package Aula02_TiposPrimitivos;
+
+public class TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        /*
+        Prática
+
+        Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+        Eu <nome>, morando no endereço <endereço>, confirmo que recebi o salário de <salário> na data <data>.
+         */
+
+        String nome = "Edgard Moraes";
+        String endereco = " Rua das amendoerias, 305 - Cosmos Rio de janeiro";
+        double salario = 1500.00;
+        String data = "24/06/2025";
+
+        System.out.println("Eu "+nome + ", morando no " +endereco + ", confirmo que recebi o salário de "+salario +" na data de "+data);
+    }
+}
