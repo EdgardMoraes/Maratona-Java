de6d64689e2b808443ace11f891340c3edb4be06
Edgard <edgardmoraes@icloud.com>
2025-06-30 19:59:32 -0300

    feat:Introdução a Arrays

---
+++ src/modulo04/Arrays.java
@@ -0,0 +1,20 @@
+package modulo04;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int [] idades = new int[3];     //  int [] idades cria um array chamado idades e new int[3] define o lenth do array.
+        System.out.println(idades);    // ao imprimir o array idades, o que irá aparecer é um endereço de memória ex. I@6acbcfc0
+
+        System.out.println(idades[0]); // irá imprimir o índice ou posição doarray, lembrando que começa com 0. Como o array foi
+                                       // inicializado porém não foi atribuido nenhum valor, será impresso 0.
+
+        idades[0] = 20;
+        idades[1] = 30;                 // array [i] foi atribuido valores ao array
+        idades[2] = 40;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);  // Agora ao imprimir será impresso os valores atribuidos 20, 30, 40
+        System.out.println(idades[2]);
+    }
+}
+
